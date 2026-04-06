aee657c0aa553fea1a4693f5e5b32ff2125e96bc
Devendradhote001 <ddhote780@gmail.com>
2026-04-06 19:44:53 +0530

    hello

---
+++ context-api-1/src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { ContextProvider, Counter } from "./context/CountContext";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+const App = () => {
+  // console.log("app rendering...");
+  // let { count, setCount } = useContext(Counter);
+
+  return (
+    <div>
+      <h1>App</h1>
+      <h1>Count is</h1>
+      {/* <button onClick={() => setCount((prev) => prev + 1)}>Increment</button> */}
+
+      <ContextProvider>
+        <About />
+        <Contact />
+      </ContextProvider>
+    </div>
+  );
+};
+
+export default App;
---
+++ context-api-1/src/main.jsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ContextProvider } from "./context/CountContext.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
