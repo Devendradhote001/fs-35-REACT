1789b1cd0613dfb27f28d61e2f478e4fee46c901
Devendradhote001 <ddhote780@gmail.com>
2026-05-04 20:01:01 +0530

    redux actions and value bhejna

--- react-redux/src/App.jsx
+++ react-redux/src/App.jsx
@@ -1,13 +1,32 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementByValue,
+} from "./features/counterReducer";
 
 const App = () => {
+  let dispatch = useDispatch();
+  const [value, setValue] = useState(0);
   let { count } = useSelector((store) => store.counter);
 
   return (
     <div>
-      <h1>React redux</h1>
+      <h1>React Redux</h1>
       <h1>Count is {count}</h1>
+
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <input
+        onChange={(e) => setValue(e.target.value)}
+        type="text"
+        placeholder="Number"
+      />
+
+      <button onClick={() => dispatch(incrementByValue(value))}>
+        Add value
+      </button>
     </div>
   );
 };
--- react-redux/src/features/counterReducer.jsx
+++ react-redux/src/features/counterReducer.jsx
@@ -3,16 +3,25 @@ import { createSlice } from "@reduxjs/toolkit";
 let counterSlice = createSlice({
   name: "count",
   initialState: {
-    count: 20,
+    count: 0,
   },
   reducers: {
     increment: (state) => {
       state.count++;
     },
     decrement: (state) => {
+      if (state.count === 0) {
+        return;
+      }
       state.count--;
+    },
+
+    incrementByValue: (state, action) => {
+      state.count += Number(action.payload);
     },
   },
 });
 
-export default counterSlice.reducer
+export let { increment, decrement, incrementByValue } = counterSlice.actions;
+
+export default counterSlice.reducer;
